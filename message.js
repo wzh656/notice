1cbbaa7425ebedff20e88e5f4edbb93716a5e70f
wzh656 <60345146+wzh656@users.noreply.github.com>
2020-02-09 13:37:53 +0800

    Update message.js

--- message.js
+++ message.js
@@ -1,4 +1,5 @@
-callback={
+callback(
+{
 	"倒计时":{
 		"new-year(2020)":new Date(2020,0,1,0,0,0,0),
 		"exam":new Date(2020,0,13,8,0,0,0),
@@ -8,4 +9,5 @@ callback={
 		"high-exam":new Date(2024,6,1,8,0,0,0),
 		"die-time":new Date(2106,9,1,12,0,0,0)
 	}
-};
+}
+);
