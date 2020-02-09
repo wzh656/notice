56ca03b8052a00ba99295b315a1eed33e260e276
wzh656 <60345146+wzh656@users.noreply.github.com>
2020-02-09 13:34:17 +0800

    Update and rename message.json to message.js

---
+++ message.js
@@ -0,0 +1,11 @@
+callback={
+	"倒计时":{
+		"new-year(2020)":new Date(2020,0,1,0,0,0,0),
+		"exam":new Date(2020,0,13,8,0,0,0),
+		"spring-festival":new Date(2020,0,25,0,0,0,0),
+		"the-end-of-winter-vacation":new Date(2020,2,1,8,0,0,0),
+		"middle-exam":new Date(2021,6,1,8,0,0,0),
+		"high-exam":new Date(2024,6,1,8,0,0,0),
+		"die-time":new Date(2106,9,1,12,0,0,0)
+	}
+};
