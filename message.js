29cbb95f95c2638e5d040e1bb52241a370459acb
wzh656 <60345146+wzh656@users.noreply.github.com>
2020-02-19 22:53:57 -0600

    Update message.js
    No，开学考

--- message.js
+++ message.js
@@ -1,14 +1,14 @@
 {
 	"倒计时":{
 		"date":{
-			"2020-new-year":new Date(2020,0,1,0,0,0,0),
-			"8.1-final-exam":new Date(2020,0,13,8,0,0,0),
-			"2020-spring-festival":new Date(2020,0,25,0,0,0,0),
-			"the-end-of-8.1-winter-vacation":new Date(2020,2,1,8,0,0,0),
-			"middle-exam":new Date(2021,6,1,8,0,0,0),
-			"high-exam":new Date(2024,6,1,8,0,0,0),
-			"die-time":new Date(2106,9,1,12,0,0,0)
+			"元旦（2020）":new Date(2020,0,1,0,0,0,0),
+			"期末考试（八下）":new Date(2020,0,13,8,0,0,0),
+			"春节（2020）":new Date(2020,0,25,0,0,0,0),
+			"寒假结束（八），And<b style='color:red;'>开～学～考～<b>":new Date(2020,2,1,8,0,0,0),
+			"中考":new Date(2021,6,1,8,0,0,0),
+			"高考":new Date(2024,6,1,8,0,0,0),
+			"desk-good死亡倒计时":new Date(2106,9,1,12,0,0,0)
 		},
-		"message":"疫情拐点已过，When 开学？"
+		"message":"No,即将开～学～考～！\n怎么办，凉拌炒鸡蛋，不好吃也不好看，放点砒霜，吃了就上天堂"
 	}
 }
