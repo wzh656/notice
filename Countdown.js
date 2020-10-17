d6d5024a6078668b5e521ff419aee9f42bc96f64
wzh656 <60345146+wzh656@users.noreply.github.com>
2020-10-17 13:16:12 +0800

    体育节 期中考试 color

--- Countdown.js
+++ Countdown.js
@@ -3,109 +3,121 @@
 		"元旦（2020）": {
 			"start": new Date(2020,0,1,0,0,0,0),
 			"end": new Date(2020,0,1,0,0,0,0),
-			"color": "#0ff",
+			"color": "#0f0",
 			"background-color": "#f00"
 		},
 		"期末考试（八上）": {
 			"start": new Date(2020,0,13,8,0,0,0),
 			"end": new Date(2020,0,15,17,0,0,0),
-			"color": "#59c",
+			"color": "#4D99FF",
 			"background-color": "#a63"
 		},
 		"春节（2020）": {
 			"start": new Date(2020,0,25,0,0,0,0),
 			"end": new Date(2020,0,25,0,0,0,0),
-			"color": "#0ff",
+			"color": "#0f0",
 			"background-color": "#f00"
 		},
 		"<b style='color:red;'>线上考试*1！</b>": {
 			"start": new Date(2020,2,7,9,0,0,0),
 			"end": new Date(2020,2,8,20,0,0,0),
-			"color": "#59c",
+			"color": "#4D99FF",
 			"background-color": "#a63"
 		},
 		"<b style='color:red;'>线上考试（月考）*2！！</b>": {
 			"start": new Date(2020,2,24,19,0,0,0),
 			"end": new Date(2020,2,29,21,10,0,0),
-			"color": "#59c",
+			"color": "#4D99FF",
 			"background-color": "#a63"
 		},
 		"<b style='color:red;'>线上考试*3！！！</b>": {
 			"start": new Date(2020,3,13,19,0,0,0),
 			"end": new Date(2020,3,18,21,10,0,0),
-			"color": "#59c",
+			"color": "#4D99FF",
 			"background-color": "#a63"
 		},
 		"<h3 style='color:red;'>线上考试*4！！！！！！</h3>": {
 			"start": new Date(2020,3,25,8,20,0,0),
 			"end": new Date(2020,3,28,21,0,0,0),
-			"color": "#59c",
+			"color": "#4D99FF",
 			"background-color": "#a63"
 		},
 		"五一假期（2020）": {
 			"start": new Date(2020,4,1,0,0,0,0),
 			"end": new Date(2020,4,4,24,0,0,0),
-			"color": "#0ff",
+			"color": "#0f0",
 			"background-color": "#f00"
 		},
 		"<h3 style='color:red;'>线上考试*5！！！！！！</h3>": {
 			"start": new Date(2020,4,5,8,0,0,0),
 			"end": new Date(2020,4,5,17,40,0,0),
-			"color": "#59c",
+			"color": "#4D99FF",
 			"background-color": "#a63"
 		},
 		"开学（八下）（<h3 style='color:red;display:inline;'>开学考！！！！！！</h3>）": {
 			"start": new Date(2020,4,11,8,0,0,0),
 			"end": new Date(2020,4,11,8,0,0,0),
-			"color": "#59c",
+			"color": "#4D99FF",
 			"background-color": "#a63"
 		},
 		"<h3 style='color:red;'>期中考（八下）！！！！！！</h3>": {
 			"start": new Date(2020,5,4,9,30,0,0),
 			"end": new Date(2020,5,5,15,55,0,0),
-			"color": "#59c",
+			"color": "#4D99FF",
 			"background-color": "#a63"
 		},
 		"<h3 style='color:red;'>学科素养考试（八下）！！！</h3>": {
 			"start": new Date(2020,5,15,8,30,0,0),
 			"end": new Date(2020,5,15,17,50,0,0),
-			"color": "#59c",
+			"color": "#4D99FF",
 			"background-color": "#a63"
 		},
 		"<h3 style='color:red;'>期末考试（八下）！！！！！！</h3>": {
 			"start": new Date(2020,6,10,8,30,0,0),
 			"end": new Date(2020,6,12,16,10,0,0),
-			"color": "#59c",
+			"color": "#4D99FF",
 			"background-color": "#a63"
 		},
 		"暑假（2020）": {
 			"start": new Date(2020,6,16,0,0,0,0),
 			"end": new Date(2020,7,31,8,0,0,0),
-			"color": "#0ff",
-			"background-color": "#f00"
+			"color": "#0f4",
+			"background-color": "#f04"
 		},
 		"<h3 style='color:red;'>第一次月考（九下）！</h3>": {
 			"start": new Date(2020,8,27,8,40,0,0),
 			"end": new Date(2020,8,28,17,0,0,0),
-			"color": "#59c",
+			"color": "#4D99FF",
 			"background-color": "#a63"
 		},
 		"生日（2020）": {
 			"start": new Date(2020,9,1,0,0,0,0),
 			"end": new Date(2020,9,1,24,0,0,0),
-			"color": "#73c",
-			"background-color": "#8c3"
+			"color": "#3f6",
+			"background-color": "#f36"
+		},
+		"体育节（九）": {
+			"start": new Date(2020,9,28,9,0,0,0),
+			"end": new Date(2020,9,30,17,0,0,0),
+			"color": "#0f0",
+			"background-color": "#f00"
+		},
+		"期中考试": {
+			"start": new Date(2021,11,11,8,10,0,0),
+			"end": new Date(2021,11,12,17,30,0,0),
+			"color": "#4D99FF",
+			"background-color": "#a63"
 		},
 		"中考": {
 			"start": new Date(2021,5,17,8,0,0,0),
 			"end": new Date(2021,5,19,12,0,0,0),
-			"color": "#59c",
+			"color": "#4D99FF",
 			"background-color": "#a63"
 		},
 		"高考": {
 			"start": new Date(2024,5,4,8,0,0,0),
 			"end": new Date(2024,5,6,12,0,0,0),
-			"color": "#59c",
+			"color": "#4D99FF",
 			"background-color": "#a63"
 		},
 		"desk-good死亡": {
@@ -115,6 +127,6 @@
 			"background-color": "#000"
 		}
 	},
-	"color": 14,
-	"message": `No,即将马上第一次月考了！然后马上只有${(new Date(2021,5,17,8)-new Date)/1000/3600/24}天就要中考了！！！\nAnd桌子好又双叒叕要跑1500光年+下雨天看树和草了！怎么办，凉拌炒鸡蛋，不好吃也不好看，放点砒霜，吃了就上天堂……`
+	"color": 17,
+	"message": `即将体育节了,deskgood终于可以跑1500了！Oh No,只有${(new Date(2021,11,11,8,10)-new Date)/1000/3600/24}天期中考试了！然后马上只有${(new Date(2021,5,17,8)-new Date)/1000/3600/24}天就要中考了！！！\nAnd桌子好又双叒叕要跑1500光年+下雨天看树和草了！怎么办，凉拌炒鸡蛋，不好吃也不好看，放点砒霜，吃了就上天堂……`
 }
